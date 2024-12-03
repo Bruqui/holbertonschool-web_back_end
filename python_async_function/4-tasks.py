8cf203638656bbab2843cf69ea60f8952e8e6537
Bruqui <dylan2003gd@gmail.com>
2024-12-03 15:10:43 +0100

    4. Tasks

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,25 +1,14 @@
 #!/usr/bin/env python3
-"""
-This file contains a function to create and return asyncio tasks for
-wait_random.
-"""
-
-import asyncio
+"""function that waits for a random delay"""
 from typing import List
-task_wait_random = __import__('3-tasks').task_wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """
-    Asynchronously runs task_wait_random n times with a specified max_delay.
-    Args:
-        n (int): The number of tasks to execute.
-        max_delay (int): The maximum delay for each task.
-    Returns:
-        list: A list of random delays in ascending order without using sort().
-    """
-    listounette = []
-    for i in range(n):
-        listounette.append(task_wait_random(max_delay))
-    sor = await asyncio.gather(*listounette)
-    return sorted(sor)
+    """Function that returns a list of deadlines in ascending order,
+    using asyncio tasks"""
+    gecikme = []
+    for _ in range(n):
+        notdelay = await wait_random(max_delay)
+        gecikme.append(notdelay)
+    return sorted(gecikme)
