8cc46fdca590ea9afbfa0e3ed0f4cc8f62e7b5c4
Bruqui <dylan2003gd@gmail.com>
2024-12-10 16:50:06 +0100

    add comment

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -14,6 +14,7 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
         page (int): The current page number.
         page_size (int): The number of items per page.
     Returns:
+        Tuple[int, int]: A tuple containing the start index and end index.
     """
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
