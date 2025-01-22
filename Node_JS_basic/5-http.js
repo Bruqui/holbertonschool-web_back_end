140b48a110af42fe264ec85fe76dc27c4ddf90c4
Bruqui <dylan2003gd@gmail.com>
2025-01-22 19:09:01 +0100

    5. Create a more complex HTTP server using Node's HTTP module

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,34 +1,33 @@
 const http = require('http');
-
 const countStudents = require('./3-read_file_async');
 
-const filename = process.argv[2];
+const app = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.write('This is the list of our students\n');
 
-const port = 1245;
-
-const app = http.createServer(async (request, response) => {
-  response.writeHead(200, {
-    'Content-Type': 'text/html',
-  });
-
-  if (request.url === '/') {
-    response.write('Hello Holberton School!');
-    response.end();
-  } else if (request.url === '/students') {
-    response.write('This is the list of our students\n');
-
-    try {
-      const students = await countStudents(filename);
-      response.end(`${students.join('\n')}`);
-    } catch (error) {
-      response.end(error.message);
-    }
+    const databasePath = process.argv[2];
+    countStudents(databasePath)
+      .then((result) => {
+        res.end(result);
+      })
+      .catch((error) => {
+        res.end(error.message);
+      });
   } else {
-    response.statusCode = 404;
-    response.end('Not Found\n');
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Not Found');
   }
 });
 
-app.listen(port);
+app.listen(1245, () => {
+  console.log('Server is listening on port 1245');
+});
 
 module.exports = app;
