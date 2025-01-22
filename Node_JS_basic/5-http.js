0d6f9432f7b97bba4146d5c727cd731b539b68ee
Bruqui <dylan2003gd@gmail.com>
2025-01-22 19:23:54 +0100

    5. Create a more complex HTTP server using Node's HTTP module

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,34 +1,25 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
+const hostname = '127.0.0.1';
 const port = 1245;
+const DB = process.argv[2];
 
 const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   if (req.url === '/') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-
-    countStudents(process.argv[2])
-      .then((response) => {
-        res.end(`This is the list of our students\n${response}`);
-      })
-      .catch((error) => {
-        res.statusCode = 500;
-        res.end(error.message);
-      });
-  } else {
-    res.statusCode = 404;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Not found.');
+    res.write('This is the list of our students\n');
+    countStudents(DB).then((result) => {
+      res.end(result.join('\n'));
+    }).catch((error) => {
+      res.end(`${error.message}`);
+    });
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server listen on port ${port}`);
-});
+app.listen(port, hostname);
 
 module.exports = app;
