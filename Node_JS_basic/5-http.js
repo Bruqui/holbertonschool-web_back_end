f45f3a837156a18196218a1f253a84ef8e88755c
Bruqui <dylan2003gd@gmail.com>
2025-01-22 19:11:35 +0100

    5. Create a more complex HTTP server using Node's HTTP module

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,6 +1,8 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
+const port = 1245;
+
 const app = http.createServer((req, res) => {
   if (req.url === '/') {
     res.statusCode = 200;
@@ -9,25 +11,24 @@ const app = http.createServer((req, res) => {
   } else if (req.url === '/students') {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.write('This is the list of our students\n');
 
-    const databasePath = process.argv[2];
-    countStudents(databasePath)
-      .then((result) => {
-        res.end(result);
+    countStudents(process.argv[2])
+      .then((response) => {
+        res.end(`This is the list of our students\n${response}`);
       })
       .catch((error) => {
+        res.statusCode = 500;
         res.end(error.message);
       });
   } else {
     res.statusCode = 404;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Not Found');
+    res.end('Not found.');
   }
 });
 
-app.listen(1245, () => {
-  console.log('Server is listening on port 1245');
+app.listen(port, () => {
+  console.log(`Server listen on port ${port}`);
 });
 
 module.exports = app;
