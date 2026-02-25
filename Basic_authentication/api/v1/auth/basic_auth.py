730d18d7c2402f2143f55639710cb9ffa5a2dbe0
Bruqui <dylan2003gd@gmail.com>
2026-02-25 21:56:11 +0100

    11. Basic - Overload current_user - and BOOM!

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -81,3 +81,27 @@ class BasicAuth(Auth):
             return None
 
         return user
+    
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Overloads Auth and retrieves the User instance for a request.
+        """
+        auth_header = self.authorization_header(request)
+        if auth_header is None:
+            return None
+
+        b64_header = self.extract_base64_authorization_header(auth_header)
+        if b64_header is None:
+            return None
+
+        decoded_header = self.decode_base64_authorization_header(b64_header)
+        if decoded_header is None:
+            return None
+
+        email, password = self.extract_user_credentials(decoded_header)
+        if email is None or password is None:
+            return None
+
+        return self.user_object_from_credentials(email, password)
+
