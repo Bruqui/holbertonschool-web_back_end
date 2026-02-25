8f99ffce090287a9536198b588d9f6a04b8fedad
Bruqui <dylan2003gd@gmail.com>
2026-02-25 11:13:03 +0100

    1. Error handler: Unauthorized

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,10 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """
+    Endpoint that raises a 401 error.
+    """
+    abort(401)
