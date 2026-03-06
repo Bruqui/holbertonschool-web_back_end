9372c664965d4f1e4f5295c1b7046bfcca834447
Bruqui <dylan2003gd@gmail.com>
2026-03-06 22:13:33 +0100

    7. Register user

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,9 +2,11 @@
 """
 Module pour l'application Flask de base.
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -17,6 +19,26 @@ def index() -> str:
         str: Un payload JSON contenant un message de bienvenue.
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users() -> str:
+    """
+    Route POST pour enregistrer un nouvel utilisateur.
+
+    Attend les champs de formulaire 'email' et 'password'.
+
+    Returns:
+        str: Un payload JSON indiquant le succès ou l'échec de la création.
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
