dcda3238502c75dbeb6d4510101d3360ed36a4e3
Bruqui <dylan2003gd@gmail.com>
2026-03-06 22:47:21 +0100

    19. Update password end-point

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -125,5 +125,29 @@ def get_reset_password_token() -> str:
         abort(403)
 
 
+@app.route("/reset_password", methods=["PUT"], strict_slashes=False)
+def update_password() -> str:
+    """
+    Route PUT pour mettre à jour le mot de passe d'un utilisateur.
+
+    Attend les champs de formulaire 'email', 'reset_token' et 'new_password'.
+
+    Returns:
+        str: Un payload JSON confirmant la mise à jour du mot de passe
+        (HTTP 200),
+             ou une erreur HTTP 403 si le jeton est invalide.
+    """
+    email = request.form.get("email")
+    reset_token = request.form.get("reset_token")
+    new_password = request.form.get("new_password")
+
+    try:
+        AUTH.update_password(reset_token, new_password)
+    except ValueError:
+        abort(403)
+
+    return jsonify({"email": email, "message": "Password updated"})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
