d8a5008f0f324d833deb16167c6cf483c078d338
Bruqui <dylan2003gd@gmail.com>
2026-03-06 22:26:52 +0100

    12. Find user by session ID

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -115,3 +115,22 @@ class Auth:
         self._db.update_user(user.id, session_id=session_id)
 
         return session_id
+
+    def get_user_from_session_id(self, session_id: str = None) -> User:
+        """
+        Récupère un utilisateur en fonction de son ID de session.
+
+        Args:
+            session_id (str): L'ID de session de l'utilisateur.
+
+        Returns:
+            User: L'objet User correspondant, ou None si l'ID est invalide
+                  ou si aucun utilisateur n'est trouvé.
+        """
+        if session_id is None:
+            return None
+
+        try:
+            return self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
