4d760f67c1be208b9f0c01997176fae1310ac9db
Bruqui <dylan2003gd@gmail.com>
2026-03-06 09:58:46 +0100

    1. Empty session

---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module for Session Authentication.
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    SessionAuth class that inherits from Auth.
+    For now, it's an empty mechanism.
+    """
+    pass
