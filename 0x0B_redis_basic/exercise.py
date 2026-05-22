66e5e80882b18091dd807c1e63b667c3e9a90536
Bruqui <dylan2003gd@gmail.com>
2026-05-22 10:06:07 +0200

    4. Retrieving lists

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -91,8 +91,8 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    @count_calls
     @call_history
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Generates a random UUID string key, stores the provided data
