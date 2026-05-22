292cfe68c7e17f3be96b87b3fb36702fe4d56d04
Bruqui <dylan2003gd@gmail.com>
2026-05-22 10:02:54 +0200

    3. Storing lists

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """
 This module provides a Cache class that interacts with a Redis data store.
-It includes decorators to track performance metrics like call counts.
+It includes decorators to track performance metrics like call counts and
+execution history.
 """
 from functools import wraps
 import redis
@@ -25,11 +26,33 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """
+    A decorator that stores the history of inputs and outputs for a
+    particular function within Redis lists.
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs) -> Any:
+        """
+        Appends the string representation of arguments to the inputs list
+        and the final return value to the outputs list in Redis.
+        """
+        input_key = f"{method.__qualname__}:inputs"
+        output_key = f"{method.__qualname__}:outputs"
+
+        self._redis.rpush(input_key, str(args))
+        output = method(self, *args, **kwargs)
+        self._redis.rpush(output_key, str(output))
+
+        return output
+    return wrapper
+
+
 class Cache:
     """
     Cache class for managing data storage inside a Redis database instance.
     It handles initialization, database flushing, data persistence, and
-    tracking execution counts.
+    tracking execution counts and histories.
     """
 
     def __init__(self) -> None:
@@ -41,6 +64,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Generates a random UUID string key, stores the provided data
